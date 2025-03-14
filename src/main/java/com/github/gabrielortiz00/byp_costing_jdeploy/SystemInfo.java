2f5f6e8e8b559175830c23db54a453515b3c78f7
gabrielortiz <gabrielortiz@192.168.11.28>
2025-03-13 23:40:03 -0600

    Commit message goes here

---
+++ src/main/java/com/github/gabrielortiz00/byp_costing_jdeploy/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.github.gabrielortiz00.byp_costing_jdeploy;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.github.gabrielortiz00.byp_costing_jdeploy {
+    requires javafx.controls;
+    exports com.github.gabrielortiz00.byp_costing_jdeploy;
+}
